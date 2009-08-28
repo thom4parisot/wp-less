546da99406eab5e54487ba45090379788b4e7bc0
oncletom <oncletom@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-28 12:14:15 +0000

    * future 1.2 version
    git-svn-id: http://plugins.svn.wordpress.org/wp-less/trunk@149986 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/Configuration.class.php
+++ lib/Configuration.class.php
@@ -10,7 +10,7 @@ class WPLessConfiguration extends WPPluginToolkitConfiguration
   /**
    * Refers to the version of the plugin
    */
-  const VERSION =   '1.1';
+  const VERSION =   '1.2-dev';
 
 
   protected function configure()
