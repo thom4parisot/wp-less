3e17e5d1b780c6a0151eec14cfb30d06f1c4a7a0
oncletom <thomas@oncle-tom.net>
2012-10-07 12:23:29 +0200

    Introducing a new compilation strategy

--- lib/Configuration.class.php
+++ lib/Configuration.class.php
@@ -18,14 +18,73 @@ class WPLessConfiguration extends WPPluginToolkitConfiguration
    */
   protected $functions = array();
 
+	/**
+	 * @protected
+	 * @deprecated
+	 * @var bool
+	 */
+	protected $alwaysRecompile = false;
+
 
   protected function configure()
   {
-	  $this->configureOptions();
+    $this->configureOptions();
   }
 
-	protected function configureOptions()
+  protected function configureOptions()
+  {
+    $this->setVariables(array());
+  }
+
+  /**
+   * Set global Less variables
+   *
+   * @since 1.4
+   */
+  public function addVariable($name, $value)
+  {
+    $this->variables[$name] = $value;
+  }
+
+  /**
+   * Returns the registered variables
+   *
+   * @since 1.4
+   * @return array
+   */
+  public function getVariables()
+  {
+    return $this->variables;
+  }
+
+  /**
+   * Set global Less variables
+   *
+   * @since 1.4
+   */
+  public function setVariables(array $variables)
+  {
+    $this->variables = $variables;
+  }
+
+	/**
+	 * Set compilation strategy
+	 *
+	 * @param $bFlag bool
+	 */
+	public function alwaysRecompile($bFlag)
 	{
-		//
+		$this->alwaysRecompile = !!$bFlag;
+	}
+
+  /**
+   * Return LESS functions
+   *
+   * @since 1.4.2
+   * @return array
+   */
+  public function getFunctions()
+  {
+    return $this->functions;
 	}
 }
