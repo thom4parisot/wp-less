05b6b786a67071314946414152850b655bce300e
RixTox <me@rixtox.com>
2012-11-11 23:08:14 +0800

    Fix load problem under Wordpress 3.4.2

--- lib/Plugin.class.php
+++ lib/Plugin.class.php
@@ -247,7 +247,7 @@ class WPLessPlugin extends WPPluginToolkitPlugin
     if (!is_admin())
     {
       do_action('wp-less_init', $this);
-      add_action('wp', array($this, 'processStylesheets'), 999, 0);
+      add_action('wp_enqueue_scripts', array($this, 'processStylesheets'), 999, 0);
       add_filter('wp-less_stylesheet_save', array($this, 'filterStylesheetUri'), 10, 2);
     }
     else
