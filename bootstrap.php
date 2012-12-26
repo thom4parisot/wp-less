4ebf4a62738a5163e7a13a1940a40c00d215ed50
oncletom <thomas@oncle-tom.net>
2012-12-26 19:38:17 +0100

    Storing an identifier to avoid relying on `$WPLessPlugin` variable

--- bootstrap.php
+++ bootstrap.php
@@ -14,7 +14,7 @@ Plugin URI: http://wordpress.org/extend/plugins/wp-less/
 if (!class_exists('WPLessPlugin'))
 {
 	require dirname(__FILE__).'/lib/Plugin.class.php';
-	$WPLessPlugin = WPPluginToolkitPlugin::create('WPLess', __FILE__);
+	$WPLessPlugin = WPPluginToolkitPlugin::create('WPLess', __FILE__, 'WPLessPlugin');
 
 	register_activation_hook(__FILE__, array($WPLessPlugin, 'install'));
 	register_deactivation_hook(__FILE__, array($WPLessPlugin, 'uninstall'));
